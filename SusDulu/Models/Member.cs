468253c7b8909b51ef66a7831c57ff5648d44082
Andhika Nugraha <andhika.nugraha@gmail.com>
2013-10-03 08:17:15 +0700

    Added Member module

---
+++ SusDulu/Models/Member.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SusDulu.Models
+{
+    public class Member
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Distance_Traveled { get; set; }
+    }
+
+    public class MembersContext : DbContext
+    {
+        public DbSet<Member> Members { get; set; }
+    }
+}
