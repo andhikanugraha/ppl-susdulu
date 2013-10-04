d6f29e18da816c24f50a692f14ff5ee0f51fcf1f
Andhika Nugraha <andhika.nugraha@gmail.com>
2013-10-04 09:04:34 +0700

    Implemented SusDuluMembershipProvider

--- SusDulu/Models/AccountModels.cs
+++ SusDulu/Models/AccountModels.cs
@@ -8,34 +8,6 @@ using System.Web.Security;
 
 namespace SusDulu.Models
 {
-    public class UsersContext : DbContext
-    {
-        public UsersContext()
-            : base("DefaultConnection")
-        {
-        }
-
-        public DbSet<UserProfile> UserProfiles { get; set; }
-    }
-
-    [Table("UserProfile")]
-    public class UserProfile
-    {
-        [Key]
-        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
-        public int UserId { get; set; }
-        public string UserName { get; set; }
-    }
-
-    public class RegisterExternalLoginModel
-    {
-        [Required]
-        [Display(Name = "User name")]
-        public string UserName { get; set; }
-
-        public string ExternalLoginData { get; set; }
-    }
-
     public class LocalPasswordModel
     {
         [Required]
--- SusDulu/Models/User.cs
+++ SusDulu/Models/User.cs
@@ -5,6 +5,7 @@ using System.Data.Entity;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using SusDulu.Helpers;
 
 namespace SusDulu.Models
 {
@@ -14,8 +15,8 @@ namespace SusDulu.Models
         [Key]
         public int ID { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
-        [NotMapped, Compare("password", ErrorMessage = "Password doesn't match")]
+        public string Password { get; set; } // hashed using PBKDF2
+        [NotMapped, Compare("Password", ErrorMessage = "Password doesn't match")]
         public string Confirm_password { get; set; }
         public string First_name { get; set; }
         public string Middle_name { get; set; }
@@ -28,5 +29,64 @@ namespace SusDulu.Models
         public string Gender { get; set; }
         public int Total_miles { get; set; }
         public int Current_miles { get; set; }
+
+        public User(string email, string unhashedPassword)
+        {
+            Email = email;
+            SetUnhashedPassword(unhashedPassword);
+        }
+
+        public void SetUnhashedPassword(string newUnhashedPassword)
+        {
+            Password = PasswordHasher.CreateHash(newUnhashedPassword);
+        }
+    }
+
+    public class UsersContext : DbContext
+    {
+        public UsersContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public User GetUser(string email)
+        {
+            return Users.Where<User>(u => u.Email == email).FirstOrDefault();
+        }
+
+        public User GetUser(int id)
+        {
+            return Users.Find(id);
+        }
+
+        public User GetUser(object obj)
+        {
+            return Users.Find(Convert.ToInt32(obj));
+        }
+
+        public User GetUser(string email, string unhashedPassword)
+        {
+            User user = GetUser(email);
+
+            if (user != null)
+            {
+                // User found. Retrieve password hash.
+                string correctHashedPassword = user.Password;
+                bool valid = PasswordHasher.ValidatePassword(unhashedPassword, correctHashedPassword);
+
+                return user;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public bool ValidateUser(string email, string unhashedPassword)
+        {
+            return (GetUser(email, unhashedPassword) != null);
+        }
     }
 }
