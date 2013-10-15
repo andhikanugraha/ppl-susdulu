df74491473e449d5eae9c98402075e61e88279f9
13510103 <johan.ridho@gmail.com>
2013-10-16 00:12:58 +0700

    tambah halaman list tiket, edit web.config jd ga pake pwd

--- SusDulu/Models/Ticket.cs
+++ SusDulu/Models/Ticket.cs
@@ -42,6 +42,10 @@ namespace SusDulu.Models
         public int Price { get; set; }
         public string Seat { get; set; }
 
+        public Ticket()
+        {
+        }
+
         public Ticket(int ID, int ID_user, int ID_flight, string Email, string First_name, string Middle_name, string Last_name, string Address, string Phone, string Gender, string City, string Province, string Postcode, string Class, int Price, string Seat)
         {
             this.ID = ID;
