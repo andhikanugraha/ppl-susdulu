0572fe9440141e4e5ffd1501ebf05161a2c8e702
Andhika Nugraha <andhika.nugraha@gmail.com>
2013-11-04 10:04:44 +0700

    Fixed PaymentController
    Payment/Index now works properly

--- SusDulu/Controllers/PaymentController.cs
+++ SusDulu/Controllers/PaymentController.cs
@@ -20,77 +20,122 @@ namespace SusDulu.Controllers
 
         public ActionResult Index()
         {
-            List<string> emailL = (List<string>)TempData["email"];
-            List<string> firstL = (List<string>)TempData["first"];
-            List<string> middleL = (List<string>)TempData["middle"];
-            List<string> lastL = (List<string>)TempData["last"];
-            List<string> addressL = (List<string>)TempData["address"];
-            List<string> phoneL = (List<string>)TempData["phone"];
-            List<string> genderL = (List<string>)TempData["gender"];
-            List<string> cityL = (List<string>)TempData["city"];
-            List<string> provinceL = (List<string>)TempData["province"];
-            List<string> postcodeL = (List<string>)TempData["postcode"];
-            List<string> classL = (List<string>)TempData["class"];
-            List<int> priceL = (List<int>)TempData["price"];
-            List<string> seatL = (List<string>)TempData["seat"];
-
-            int id_flight1 = int.Parse(TempData["idFlight1"].ToString());
-            int id_flight2 = int.Parse(TempData["idFlight2"].ToString());
-            int sum = int.Parse(TempData["sum"].ToString());
-            if (id_flight2 != 0)
+            if (TempData["idFlight1"] == null)
             {
-                sum *= 2;
+                ViewBag.Error = true;
             }
-
-            //generate ID Ticket auto-increment
-            List<int> tickIDList = new List<int>();
-            var tickIDQry = from t in db.Tickets
-                            select t.ID;
-            tickIDList.AddRange(tickIDQry.Distinct());
-            int lastElmt;
-            try
+            else
             {
-                lastElmt = tickIDList.Last();
-            }
-            catch (InvalidOperationException e)
-            {
-                lastElmt = 0;
-            }
-            int newID = lastElmt + 1;
+                ViewBag.Error = false;
+                List<string> emailL = (List<string>)TempData["email"];
+                List<string> firstL = (List<string>)TempData["first"];
+                List<string> middleL = (List<string>)TempData["middle"];
+                List<string> lastL = (List<string>)TempData["last"];
+                List<string> addressL = (List<string>)TempData["address"];
+                List<string> phoneL = (List<string>)TempData["phone"];
+                List<string> genderL = (List<string>)TempData["gender"];
+                List<string> cityL = (List<string>)TempData["city"];
+                List<string> provinceL = (List<string>)TempData["province"];
+                List<string> postcodeL = (List<string>)TempData["postcode"];
+                List<string> classL = (List<string>)TempData["class"];
+                List<int> priceL = (List<int>)TempData["price"];
+                List<string> seatL = (List<string>)TempData["seat"];
 
-            int? IDUser = WebSecurity.CurrentUserId;
-            if (IDUser < 0) IDUser = null;
-
-            Debug.WriteLine("IDUSER: "+IDUser);
-
-            for (int k = 0; k < emailL.Count; k++)
-            {
-                if (ModelState.IsValid)
+                int id_flight1 = int.Parse(TempData["idFlight1"].ToString());
+                int id_flight2 = int.Parse(TempData["idFlight2"].ToString());
+                int sum = int.Parse(TempData["sum"].ToString());
+                if (id_flight2 != 0)
                 {
-                    //get certain IDUser
-                    List<int> userIDList = new List<int>();
-
-                    Ticket newTicket = new Ticket(newID, IDUser, id_flight1, emailL.ElementAt(k), firstL.ElementAt(k), middleL.ElementAt(k), lastL.ElementAt(k), addressL.ElementAt(k), phoneL.ElementAt(k), genderL.ElementAt(k), cityL.ElementAt(k), provinceL.ElementAt(k), postcodeL.ElementAt(k), classL.ElementAt(k), priceL.ElementAt(k), seatL.ElementAt(k));
-                    CommitDB(id_flight1, newTicket);
-
-                    newTicket = new Ticket(newID, IDUser, id_flight2, emailL.ElementAt(k), firstL.ElementAt(k), middleL.ElementAt(k), lastL.ElementAt(k), addressL.ElementAt(k), phoneL.ElementAt(k), genderL.ElementAt(k), cityL.ElementAt(k), provinceL.ElementAt(k), postcodeL.ElementAt(k), classL.ElementAt(k), priceL.ElementAt(k), seatL.ElementAt(k));
-                    CommitDB(id_flight2,newTicket);
+                    sum *= 2;
                 }
-            }
 
-            ViewBag.sum = sum;
-            ViewBag.firstID = newID;
-            return View();
-        }
+                //generate ID Ticket auto-increment
+                List<int> tickIDList = new List<int>();
+                var tickIDQry = from t in db.Tickets
+                                select t.ID;
+                tickIDList.AddRange(tickIDQry.Distinct());
+                int lastElmt;
+                try
+                {
+                    lastElmt = tickIDList.Last();
+                }
+                catch (InvalidOperationException e)
+                {
+                    lastElmt = 0;
+                }
+                int newID = lastElmt + 1;
 
-        private void CommitDB(int IDFlight, Ticket ticket)
-        {
-            if (IDFlight != 0)
-            {
-                db.Tickets.Add(ticket);
+                int? IDUser = WebSecurity.CurrentUserId;
+                if (IDUser < 0) IDUser = null;
+
+                Debug.WriteLine("IDUSER: " + IDUser);
+
+                var tickets = new List<Ticket>();
+
+                for (int k = 0; k < emailL.Count; k++)
+                {
+                    if (ModelState.IsValid)
+                    {
+                        //get certain IDUser
+                        List<int> userIDList = new List<int>();
+
+                        Ticket departureTicket = new Ticket()
+                        {
+                            ID_user = IDUser,
+                            ID_flight = id_flight1,
+                            Email = emailL[k],
+                            First_name = firstL[k],
+                            Middle_name = middleL[k],
+                            Last_name = lastL[k],
+                            Address = addressL[k],
+                            Phone = phoneL[k],
+                            Gender = genderL[k],
+                            City = cityL[k],
+                            Province = provinceL[k],
+                            Postcode = postcodeL[k],
+                            Class = classL[k],
+                            Price = priceL[k],
+                            Seat = seatL[k]
+                        };
+
+                        db.Tickets.Add(departureTicket);
+                        tickets.Add(departureTicket);
+
+                        if (id_flight2 != 0)
+                        {
+                            Ticket returnTicket = new Ticket()
+                            {
+                                ID_user = IDUser,
+                                ID_flight = id_flight2,
+                                Email = emailL[k],
+                                First_name = firstL[k],
+                                Middle_name = middleL[k],
+                                Last_name = lastL[k],
+                                Address = addressL[k],
+                                Phone = phoneL[k],
+                                Gender = genderL[k],
+                                City = cityL[k],
+                                Province = provinceL[k],
+                                Postcode = postcodeL[k],
+                                Class = classL[k],
+                                Price = priceL[k],
+                                Seat = seatL[k]
+                            };
+
+                            db.Tickets.Add(returnTicket);
+                            tickets.Add(returnTicket);
+                        }
+                    }
+                }
+
                 db.SaveChanges();
+
+                ViewBag.sum = sum;
+                ViewBag.firstID = newID;
+                ViewBag.tickets = tickets;
             }
-            return;
+            
+            return View();
         }
 
         [AllowAnonymous]
